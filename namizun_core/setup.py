96e7a1a104247711b49c5d70c4d55b418ae4ab40
pouriasharifzad <pouria.sharifzad@gmail.com>
2025-03-19 11:45:10 +0000

    Fix setuptools discovery issue for Ubuntu 24

--- namizun_core/setup.py
+++ namizun_core/setup.py
@@ -1,13 +1,17 @@
 from setuptools import setup
 
-setup(name='namizun_core',
-      version='1.3.8',
-      description='namizun main functions',
-      author='MalKeMit',
-      author_email='khodemalkemit@gmail.com',
-      url='https://github.com/malkemit/namizun',
-      setup_requires=['wheel'],
-      install_requires=['psutil==5.9.4',
-                        'redis==4.3.5',
-                        'pytz==2022.6']
-      )
+setup(
+    name='namizun_core',
+    version='1.3.8',
+    description='namizun main functions',
+    author='MalKeMit',
+    author_email='khodemalkemit@gmail.com',
+    url='https://github.com/malkemit/namizun',
+    setup_requires=['wheel'],
+    install_requires=[
+        'psutil==5.9.4',
+        'redis==4.3.5',
+        'pytz==2022.6'
+    ],
+    py_modules=['network', 'ip', 'udp', 'time', 'log', 'database'] 
+)
